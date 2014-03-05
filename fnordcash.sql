76db0e290ec9f0f396af38b45fcd58c65d9ae211
vileda <vileda@piratenfraktion-nrw.de>
2014-03-06 00:53:02 +0100

    datentyp geändert, und foo

--- fnordcash.sql
+++ fnordcash.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.3.3
 -- Dumped by pg_dump version 9.3.3
--- Started on 2014-03-04 18:11:49 CET
+-- Started on 2014-03-06 00:50:37 CET
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -14,7 +14,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 179 (class 3079 OID 11764)
+-- TOC entry 181 (class 3079 OID 11764)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -22,8 +22,8 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 1990 (class 0 OID 0)
--- Dependencies: 179
+-- TOC entry 2000 (class 0 OID 0)
+-- Dependencies: 181
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -33,17 +33,19 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 SET search_path = public, pg_catalog;
 
 --
--- TOC entry 192 (class 1255 OID 151993)
+-- TOC entry 194 (class 1255 OID 16395)
 -- Name: get_mitglied_kontostand(integer); Type: FUNCTION; Schema: public; Owner: postgres
 --
 
-CREATE FUNCTION get_mitglied_kontostand(mitglied_id integer) RETURNS double precision
+CREATE FUNCTION get_mitglied_kontostand(mitglied_id integer) RETURNS money
     LANGUAGE sql
     AS $$ SELECT sum(sparkasse.betrag) AS kontostand
    FROM mitglieder,
     sparkasse
-  WHERE sparkasse.beguenstigter ~~* (('%'::text || mitglieder.nachname) || '%'::text)
-  AND mitglieder.id = mitglied_id;$$;
+  WHERE
+  mitglieder.id = mitglied_id
+  and sparkasse.verwendungszweck ~~* (('%'::text || mitglieder.mitgliednr) || '%'::text)
+  ;$$;
 
 
 ALTER FUNCTION public.get_mitglied_kontostand(mitglied_id integer) OWNER TO postgres;
@@ -53,7 +55,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 170 (class 1259 OID 151998)
+-- TOC entry 170 (class 1259 OID 16396)
 -- Name: sparkasse; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -67,7 +69,7 @@ CREATE TABLE sparkasse (
     beguenstigter text,
     kontonummer text,
     blz text,
-    betrag double precision,
+    betrag money,
     waehrung text,
     info text
 );
@@ -76,7 +78,7 @@ CREATE TABLE sparkasse (
 ALTER TABLE public.sparkasse OWNER TO postgres;
 
 --
--- TOC entry 171 (class 1259 OID 152004)
+-- TOC entry 171 (class 1259 OID 16402)
 -- Name: Kontostand; Type: VIEW; Schema: public; Owner: postgres
 --
 
@@ -88,7 +90,47 @@ CREATE VIEW "Kontostand" AS
 ALTER TABLE public."Kontostand" OWNER TO postgres;
 
 --
--- TOC entry 172 (class 1259 OID 152012)
+-- TOC entry 172 (class 1259 OID 16406)
+-- Name: barkasse; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE barkasse (
+    id integer NOT NULL,
+    mitglied_id integer,
+    datum date,
+    belegtext text,
+    betrag money
+);
+
+
+ALTER TABLE public.barkasse OWNER TO postgres;
+
+--
+-- TOC entry 173 (class 1259 OID 16412)
+-- Name: barkasse_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE barkasse_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.barkasse_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 2001 (class 0 OID 0)
+-- Dependencies: 173
+-- Name: barkasse_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE barkasse_id_seq OWNED BY barkasse.id;
+
+
+--
+-- TOC entry 174 (class 1259 OID 16414)
 -- Name: beitraege; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -97,14 +139,14 @@ CREATE TABLE beitraege (
     name text,
     gueltig_von date,
     gueltig_bis date,
-    beitrag double precision
+    beitrag money
 );
 
 
 ALTER TABLE public.beitraege OWNER TO postgres;
 
 --
--- TOC entry 173 (class 1259 OID 152018)
+-- TOC entry 175 (class 1259 OID 16420)
 -- Name: beitraege_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -119,8 +161,8 @@ CREATE SEQUENCE beitraege_id_seq
 ALTER TABLE public.beitraege_id_seq OWNER TO postgres;
 
 --
--- TOC entry 1991 (class 0 OID 0)
--- Dependencies: 173
+-- TOC entry 2002 (class 0 OID 0)
+-- Dependencies: 175
 -- Name: beitraege_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -128,7 +170,7 @@ ALTER SEQUENCE beitraege_id_seq OWNED BY beitraege.id;
 
 
 --
--- TOC entry 174 (class 1259 OID 152020)
+-- TOC entry 176 (class 1259 OID 16422)
 -- Name: mitglied_beitrag; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -142,7 +184,7 @@ CREATE TABLE mitglied_beitrag (
 ALTER TABLE public.mitglied_beitrag OWNER TO postgres;
 
 --
--- TOC entry 175 (class 1259 OID 152023)
+-- TOC entry 177 (class 1259 OID 16425)
 -- Name: mitglied_beitrag_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -157,8 +199,8 @@ CREATE SEQUENCE mitglied_beitrag_id_seq
 ALTER TABLE public.mitglied_beitrag_id_seq OWNER TO postgres;
 
 --
--- TOC entry 1992 (class 0 OID 0)
--- Dependencies: 175
+-- TOC entry 2003 (class 0 OID 0)
+-- Dependencies: 177
 -- Name: mitglied_beitrag_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -166,7 +208,7 @@ ALTER SEQUENCE mitglied_beitrag_id_seq OWNED BY mitglied_beitrag.id;
 
 
 --
--- TOC entry 178 (class 1259 OID 152078)
+-- TOC entry 178 (class 1259 OID 16427)
 -- Name: mitglieder; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -214,12 +256,12 @@ CREATE TABLE mitglieder (
 ALTER TABLE public.mitglieder OWNER TO postgres;
 
 --
--- TOC entry 177 (class 1259 OID 152076)
+-- TOC entry 179 (class 1259 OID 16433)
 -- Name: mitglieder_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE mitglieder_id_seq
-    START WITH 1
+    START WITH 10000
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
@@ -229,8 +271,8 @@ CREATE SEQUENCE mitglieder_id_seq
 ALTER TABLE public.mitglieder_id_seq OWNER TO postgres;
 
 --
--- TOC entry 1993 (class 0 OID 0)
--- Dependencies: 177
+-- TOC entry 2004 (class 0 OID 0)
+-- Dependencies: 179
 -- Name: mitglieder_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -238,7 +280,7 @@ ALTER SEQUENCE mitglieder_id_seq OWNED BY mitglieder.id;
 
 
 --
--- TOC entry 176 (class 1259 OID 152033)
+-- TOC entry 180 (class 1259 OID 16435)
 -- Name: sparkasse_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -253,8 +295,8 @@ CREATE SEQUENCE sparkasse_id_seq
 ALTER TABLE public.sparkasse_id_seq OWNER TO postgres;
 
 --
--- TOC entry 1994 (class 0 OID 0)
--- Dependencies: 176
+-- TOC entry 2005 (class 0 OID 0)
+-- Dependencies: 180
 -- Name: sparkasse_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -262,7 +304,15 @@ ALTER SEQUENCE sparkasse_id_seq OWNED BY sparkasse.id;
 
 
 --
--- TOC entry 1863 (class 2604 OID 152035)
+-- TOC entry 1870 (class 2604 OID 16437)
+-- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY barkasse ALTER COLUMN id SET DEFAULT nextval('barkasse_id_seq'::regclass);
+
+
+--
+-- TOC entry 1871 (class 2604 OID 16438)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -270,7 +320,7 @@ ALTER TABLE ONLY beitraege ALTER COLUMN id SET DEFAULT nextval('beitraege_id_seq
 
 
 --
--- TOC entry 1864 (class 2604 OID 152036)
+-- TOC entry 1872 (class 2604 OID 16439)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -278,7 +328,7 @@ ALTER TABLE ONLY mitglied_beitrag ALTER COLUMN id SET DEFAULT nextval('mitglied_
 
 
 --
--- TOC entry 1865 (class 2604 OID 152081)
+-- TOC entry 1873 (class 2604 OID 16440)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -286,7 +336,7 @@ ALTER TABLE ONLY mitglieder ALTER COLUMN id SET DEFAULT nextval('mitglieder_id_s
 
 
 --
--- TOC entry 1862 (class 2604 OID 152038)
+-- TOC entry 1869 (class 2604 OID 16441)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -294,7 +344,16 @@ ALTER TABLE ONLY sparkasse ALTER COLUMN id SET DEFAULT nextval('sparkasse_id_seq
 
 
 --
--- TOC entry 1869 (class 2606 OID 152040)
+-- TOC entry 1877 (class 2606 OID 16443)
+-- Name: barkasse_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY barkasse
+    ADD CONSTRAINT barkasse_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 1879 (class 2606 OID 16445)
 -- Name: beitraege_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -303,7 +362,7 @@ ALTER TABLE ONLY beitraege
 
 
 --
--- TOC entry 1871 (class 2606 OID 152042)
+-- TOC entry 1881 (class 2606 OID 16447)
 -- Name: mitglied_beitrag_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -312,7 +371,7 @@ ALTER TABLE ONLY mitglied_beitrag
 
 
 --
--- TOC entry 1873 (class 2606 OID 152086)
+-- TOC entry 1883 (class 2606 OID 16449)
 -- Name: mitglieder_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -321,7 +380,7 @@ ALTER TABLE ONLY mitglieder
 
 
 --
--- TOC entry 1867 (class 2606 OID 152046)
+-- TOC entry 1875 (class 2606 OID 16451)
 -- Name: pk_sparkasse; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -330,7 +389,7 @@ ALTER TABLE ONLY sparkasse
 
 
 --
--- TOC entry 1874 (class 2606 OID 152047)
+-- TOC entry 1884 (class 2606 OID 16452)
 -- Name: mitglied_beitrag_beitrag_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -339,7 +398,7 @@ ALTER TABLE ONLY mitglied_beitrag
 
 
 --
--- TOC entry 1989 (class 0 OID 0)
+-- TOC entry 1999 (class 0 OID 0)
 -- Dependencies: 6
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -350,7 +409,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2014-03-04 18:11:49 CET
+-- Completed on 2014-03-06 00:50:37 CET
 
 --
 -- PostgreSQL database dump complete
