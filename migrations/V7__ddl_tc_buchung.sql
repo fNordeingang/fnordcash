060ec6643652f636c7127faff0e2fb7d0911ca51
Philipp K <hep@phoenix.uberspace.de>
2014-05-03 00:32:28 +0200

    Divided tc_buchung in new tc_buchungsposition to make split transactions possible

--- migrations/V7__ddl_tc_buchung.sql
+++ migrations/V7__ddl_tc_buchung.sql
@@ -6,9 +6,6 @@ CREATE TABLE tc_buchung
 (
   id integer NOT NULL,
   buchungsdatum timestamp with time zone NOT NULL DEFAULT now(),
-  betrag money NOT NULL,
-  sollkonto integer NOT NULL,
-  habenkonto integer NOT NULL,
   belegdatum date NOT NULL,
   belegtext text NOT NULL,
   beschreibung text,
@@ -19,23 +16,4 @@ WITH (
 );
 ALTER TABLE tc_buchung
   OWNER TO fnordcash;
-
--- Index: ix_tc_buchung_habenkonto
-
--- DROP INDEX ix_tc_buchung_habenkonto;
-
-CREATE INDEX ix_tc_buchung_habenkonto
-  ON tc_buchung
-  USING btree
-  (habenkonto);
-
--- Index: ix_tc_buchung_sollkonto
-
--- DROP INDEX ix_tc_buchung_sollkonto;
-
-CREATE INDEX ix_tc_buchung_sollkonto
-  ON tc_buchung
-  USING btree
-  (sollkonto);
-
 
