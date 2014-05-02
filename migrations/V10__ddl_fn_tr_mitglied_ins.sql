56b35296c41ff894645213a100fac7a25f72ab12
Philipp K <hep@phoenix.uberspace.de>
2014-05-02 19:22:22 +0200

    TC_MITGLIED split function

--- migrations/V10__ddl_fn_tr_mitglied_ins.sql
+++ migrations/V10__ddl_fn_tr_mitglied_ins.sql
@@ -1,4 +1,4 @@
--- Function: fn_tr_mitglied_ins()
+﻿-- Function: fn_tr_mitglied_ins()
 
 -- DROP FUNCTION fn_tr_mitglied_ins();
 
@@ -10,9 +10,9 @@ declare
   CREDCODE text := ltrim(to_char(NEW.NHID + 60000, '99999'));
 begin
   if((select count(*) from TC_KONTO where CODE = DEBICODE) = 0) then
-    insert into TC_KONTO (ID, NAME, TYPE, PARENT_ID, CODE, DESCRIPTION)
+    insert into TC_KONTO (ID, TYPE, TYPE, PARENT_ID, CODE, DESCRIPTION)
     values (nextval('SQ_KONTO_ID'), 'A', DEBICODE || ' Personenkonto Debitor',  1248, DEBICODE, 'Personenkonto Debitor '  || DEBICODE);
-    insert into TC_KONTO (ID, NAME, TYPE, PARENT_ID, CODE, DESCRIPTION)
+    insert into TC_KONTO (ID, TYPE, NAME, PARENT_ID, CODE, DESCRIPTION)
     values (nextval('SQ_KONTO_ID'), 'A', CREDCODE || ' Personenkonto Kreditor', 1249, CREDCODE, 'Personenkonto Kreditor ' || DEBICODE /* sic! */);
   end if;
   return NEW;
---
+++ sql/fct_tc_mitglied_split.sql
@@ -0,0 +1,84 @@
+﻿create or replace function fct_tc_mitglied_split(p_nhid integer, p_date date) returns void as $$
+declare
+  v_gueltig_bis date;
+begin
+  select GUELTIG_BIS into v_gueltig_bis from TC_MITGLIED where NHID = p_nhid and p_date between GUELTIG_VON and GUELTIG_BIS;
+  update TC_MITGLIED set GUELTIG_BIS = p_date - 1 where NHID = p_nhid and p_date between GUELTIG_VON and GUELTIG_BIS;
+  insert into TC_MITGLIED
+  (
+    ID,
+    NHID,
+    GUELTIG_VON,
+    GUELTIG_BIS,
+    ANREDE,
+    TITEL,
+    VORNAME,
+    NACHNAME,
+    FIRMA,
+    ABTEILUNG,
+    STRASSE,
+    PLZ,
+    ORT,
+    RESERVIERT,
+    RESERVIERT2,
+    LAND,
+    TELEFON,
+    TELEFAX,
+    EMAIL,
+    KONTONR,
+    BLZ,
+    IBAN,
+    BIC,
+    BANKNAME,
+    SEPA_MANDATSREFERENZ,
+    DATUM_UNTERSCHRIFT,
+    GEBURTSDATUM,
+    EINTRITTSDATUM,
+    AUSTRITTSDATUM,
+    BEMERKUNG,
+    TELEFON2,
+    SKYPE_VOIP,
+    INHABER,
+    BEITRAG_NHID
+  )
+  select
+    nextval('sq_mitglied_id'),
+    NHID,
+    p_date,
+    v_gueltig_bis,
+    ANREDE,
+    TITEL,
+    VORNAME,
+    NACHNAME,
+    FIRMA,
+    ABTEILUNG,
+    STRASSE,
+    PLZ,
+    ORT,
+    RESERVIERT,
+    RESERVIERT2,
+    LAND,
+    TELEFON,
+    TELEFAX,
+    EMAIL,
+    KONTONR,
+    BLZ,
+    IBAN,
+    BIC,
+    BANKNAME,
+    SEPA_MANDATSREFERENZ,
+    DATUM_UNTERSCHRIFT,
+    GEBURTSDATUM,
+    EINTRITTSDATUM,
+    AUSTRITTSDATUM,
+    BEMERKUNG,
+    TELEFON2,
+    SKYPE_VOIP,
+    INHABER,
+    BEITRAG_NHID
+  from TC_MITGLIED where NHID = p_nhid and p_date - 1 between GUELTIG_VON and GUELTIG_BIS;
+  return;
+end;
+$$ language plpgsql;
+
+--select fct_tc_mitglied_split(10008, '2014-03-01');
