df87e857e8afd6017c1af4f6512a51e525e8697f
Philipp K <hep@phoenix.uberspace.de>
2014-05-03 01:02:02 +0200

    Added reference in tc_bankbuchung to tc_buchung

--- migrations/V5__ddl_tc_bankbuchung.sql
+++ migrations/V5__ddl_tc_bankbuchung.sql
@@ -16,11 +16,25 @@ CREATE TABLE tc_bankbuchung
   betrag money,
   waehrung text,
   info text,
-  CONSTRAINT pk_tc_bankbuchung PRIMARY KEY (id)
+  buchung_id integer,
+  CONSTRAINT pk_tc_bankbuchung PRIMARY KEY (id),
+  CONSTRAINT fk_bankbuchung_buchung_id FOREIGN KEY (buchung_id)
+      REFERENCES tc_buchung (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
   OIDS=FALSE
 );
 ALTER TABLE tc_bankbuchung
   OWNER TO fnordcash;
+
+-- Index: fki_bankbuchung_buchung_id
+
+-- DROP INDEX fki_bankbuchung_buchung_id;
+
+CREATE INDEX fki_bankbuchung_buchung_id
+  ON tc_bankbuchung
+  USING btree
+  (buchung_id);
+
 
