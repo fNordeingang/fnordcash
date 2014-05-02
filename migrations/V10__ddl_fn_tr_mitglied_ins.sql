69bb71bf22b2407993e73a9ab5b600c0d9c54e9f
Philipp K <hep@phoenix.uberspace.de>
2014-05-03 00:54:10 +0200

    Only create debtor accounts for members in trigger function

--- migrations/V10__ddl_fn_tr_mitglied_ins.sql
+++ migrations/V10__ddl_fn_tr_mitglied_ins.sql
@@ -7,13 +7,13 @@ CREATE OR REPLACE FUNCTION fn_tr_mitglied_ins()
 $BODY$
 declare
   DEBICODE text := ltrim(to_char(NEW.NHID        , '99999'));
-  CREDCODE text := ltrim(to_char(NEW.NHID + 60000, '99999'));
+  --CREDCODE text := ltrim(to_char(NEW.NHID + 60000, '99999'));
 begin
   if((select count(*) from TC_KONTO where CODE = DEBICODE) = 0) then
     insert into TC_KONTO (ID, TYPE, TYPE, PARENT_ID, CODE, DESCRIPTION)
     values (nextval('SQ_KONTO_ID'), 'A', DEBICODE || ' Personenkonto Debitor',  1248, DEBICODE, 'Personenkonto Debitor '  || DEBICODE);
-    insert into TC_KONTO (ID, TYPE, NAME, PARENT_ID, CODE, DESCRIPTION)
-    values (nextval('SQ_KONTO_ID'), 'A', CREDCODE || ' Personenkonto Kreditor', 1249, CREDCODE, 'Personenkonto Kreditor ' || DEBICODE /* sic! */);
+    --insert into TC_KONTO (ID, TYPE, NAME, PARENT_ID, CODE, DESCRIPTION)
+    --values (nextval('SQ_KONTO_ID'), 'A', CREDCODE || ' Personenkonto Kreditor', 1249, CREDCODE, 'Personenkonto Kreditor ' || DEBICODE /* sic! */);
   end if;
   return NEW;
 end;
