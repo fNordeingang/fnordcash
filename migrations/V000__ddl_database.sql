3d0d53cd207c680e04fec02fd3076c6beefbd3a1
vileda <vileda@piratenfraktion-nrw.de>
2014-05-11 17:51:53 +0200

    fuxkup

--- migrations/V000__ddl_database.sql
+++ migrations/V000__ddl_database.sql
@@ -2,18 +2,18 @@
 
 -- DROP DATABASE "default";
 
-CREATE DATABASE "default"
-  WITH OWNER = fnordcash
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'de_DE.UTF-8'
-       LC_CTYPE = 'de_DE.UTF-8'
-       CONNECTION LIMIT = -1;
+-- CREATE DATABASE "default"
+--  WITH OWNER = fnordcash
+--       ENCODING = 'UTF8'
+--       TABLESPACE = pg_default
+--       LC_COLLATE = 'de_DE.UTF-8'
+--       LC_CTYPE = 'de_DE.UTF-8'
+--       CONNECTION LIMIT = -1;
 
 -- Schema: fnordcash
 
 -- DROP SCHEMA fnordcash;
 
-CREATE SCHEMA fnordcash
-  AUTHORIZATION fnordcash;
+-- CREATE SCHEMA fnordcash
+--  AUTHORIZATION fnordcash;
 
