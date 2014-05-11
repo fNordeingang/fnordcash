948363d929b35cf53b730fa6cc50d97b1d388d49
vileda <vileda@piratenfraktion-nrw.de>
2014-05-11 18:22:32 +0200

    drop trigger

--- migrations/V011__ddl_tr_mitglied_ins.sql
+++ migrations/V011__ddl_tr_mitglied_ins.sql
@@ -1,6 +1,6 @@
 -- Trigger: tr_mitglied_ins on tc_mitglied
 
--- DROP TRIGGER tr_mitglied_ins ON tc_mitglied;
+DROP TRIGGER tr_mitglied_ins ON tc_mitglied;
 
 CREATE TRIGGER tr_mitglied_ins
   AFTER INSERT
